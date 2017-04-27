0d5c88e82ade2b268a33380c5cf22bc391a21a30
Aitch <dev-aitch@hotmail.com>
2017-04-27 10:50:07 +0200

    add of Pinterest feed box, and its style

--- footer.php
+++ footer.php
@@ -60,7 +60,11 @@
 							));
 						?> <!-- .archives -->
 					</div> <!-- .archive-categories -->
-					
+					<div class="pinterest-profile">
+						<h2>Pinterest</h2>
+						<a data-pin-do="embedUser" data-pin-board-width="400" data-pin-scale-height="240" data-pin-scale-width="80" href="https://fr.pinterest.com/culturecherif/"></a>
+						<script async defer src="//assets.pinterest.com/js/pinit.js"></script>
+					</div>
 				</div><!-- .grid-container -->
 			</div><!-- #secondary  {end widget area} -->
 			<div class="footer-bottom-info">
